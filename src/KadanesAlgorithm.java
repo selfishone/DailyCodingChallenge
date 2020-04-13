ae6d4b5bd6ef6d57cc9875a0c8ab59d0799926a1
NikhilManikonda <manikondanikhil@gmail.com>
2020-04-13 13:04:13 +0530

    Updated Kadanes algorithm implementation

--- src/KadanesAlgorithm.java
+++ src/KadanesAlgorithm.java
@@ -1,18 +1,29 @@
 //Longest Continuous subArray Sum in an Array.
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// ALGORITHM:
+// Initialize:
+//        max_so_far = 0
+//        max_ending_here = 0
+//
+//        Loop for each element of the array
+//        (a) max_ending_here = max_ending_here + a[i]
+//        (b) if(max_ending_here < 0)
+//        max_ending_here = 0
+//        (c) if(max_so_far < max_ending_here)
+//        max_so_far = max_ending_here
+//        return max_so_far
 public class KadanesAlgorithm {
 
+    // Positive contiguous Sum of the array is considered wont work with array with all negative numbers
     public static Integer kadanesSum(List<Integer> input){
         int size = input.size();
-
         int largest_sum = Integer.MIN_VALUE;
         int current_sum = 0;
-        int start= 0;
-        int end = 0;
 
         for(int i=0; i< size; i++){
 
@@ -23,9 +34,20 @@ public class KadanesAlgorithm {
             if(current_sum>largest_sum){
                 largest_sum = current_sum;
             }
-
         }
+        return largest_sum;
+    }
 
+    //This implementation will work with negative arrays as well.
+    public static Integer kadanesSum2(List<Integer> input){
+        int size = input.size();
+        int largest_sum = input.get(0);
+        int current_sum = input.get(0);
+
+        for(int i=1; i< size; i++){
+            current_sum = Math.max(input.get(i), input.get(i)+current_sum);
+            largest_sum = Math.max(current_sum,largest_sum);
+        }
         return largest_sum;
     }
 
